19d733e3ec1933bd8af28a8813bb0b6fc9064b98
vbounyarith <vbounyarith@scholardevapps.com>
2022-03-26 18:39:44 -0700

    Updates

---
+++ precache-manifest.3f5a7ae687f07dbf0f7de20d5285fbd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed6fb338933bd5610883",
+    "url": "/starting-react/static/css/main.18fb8733.chunk.css"
+  },
+  {
+    "revision": "ed6fb338933bd5610883",
+    "url": "/starting-react/static/js/main.ed6fb338.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/starting-react/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "e56018d6171619ac8050",
+    "url": "/starting-react/static/js/2.e56018d6.chunk.js"
+  },
+  {
+    "revision": "ce7a1e653d1d9631c705",
+    "url": "/starting-react/static/js/runtime~main.ce7a1e65.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/starting-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "26b0363f521f38b409d3f5d312a16e8c",
+    "url": "/starting-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/starting-react/precache-manifest.3f5a7ae687f07dbf0f7de20d5285fbd0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/starting-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
